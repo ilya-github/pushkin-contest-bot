ff041d02aa0d5cc67f8e939b70049ee835bbd69c
ilya-github <kulikovitch52@mail.ru>
2017-12-22 01:24:23 +0300

    lock capistrano

--- pushkin/config/deploy.rb
+++ pushkin/config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.10.1"
+lock "~> 3.5.0"
 
 set :application, "pushkin"
 set :repo_url, "git@github.com:ilya-github/pushkin-contest-bot.git"
