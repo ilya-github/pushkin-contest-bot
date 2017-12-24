6284b1b93704f208eb1bac3889485dd2fc893836
ilya-github <kulikovitch52@mail.ru>
2017-12-24 15:59:40 +0300

    fd

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.7.0"
+lock "~> 3.10.1"
 
 set :application, "pushkin-contest-bot"
 set :repo_url, "git@github.com:ilya-github/pushkin-contest-bot.git"
@@ -25,6 +25,81 @@ set :sidekiq_role, :sidekiq
 
 set :puma_preload_app, true
 set :puma_init_active_record, true
+
+
+set :puma_bind,       "unix:///var/www/pushkin-contest-bot/tmp/sockets/puma.sock"
+set :puma_state,      "var/www/pushkin-contest-bot/tmp/pids/puma.state"
+set :puma_pid,        "var/www/pushkin-contest-bot/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+#set :ssh_options,     { forward_agent: true }
+#set :puma_preload_app, true
+set :puma_worker_timeout, nil
+#set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+
+## Defaults:
+# set :scm,           :git
+# set :branch,        :master
+# set :format,        :pretty
+# set :log_level,     :debug
+# set :keep_releases, 5
+
+## Linked Files & Directories (Default None):
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
+end
+
+namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      Rake::Task["puma:start"].reenable
+      Rake::Task["puma:restart"].reenable
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
+end
+
+# ps aux | grep puma    # Get puma pid
+# kill -s SIGUSR2 pid   # Restart puma
+# kill -s SIGTERM pid   # Stop puma
+
+
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
 
