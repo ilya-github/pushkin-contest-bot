4758e8b1ea1fc3cb11ca3d9030a51f3a3ac7fd0b
ilya-github <kulikovitch52@mail.ru>
2017-12-24 05:11:46 +0300

    s

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,101 +1,85 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.10.1"
+# Change these
 
-set :application, "pushkin-contest-bot"
-set :repo_url, "git@github.com:ilya-github/pushkin-contest-bot.git"
+set :repo_url, 'git@github.com:ilya-github/pushkin-contest-bot.git'
+set :application, 'pushkin-contest-bot'
+set :user,            'deployer'
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/www/pushkin-contest-bot"
- set :rvm_ruby_version, '2.4.1@pushkin-contest-bot'
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
- append :linked_files, "config/database.yml", "config/secrets.yml"
-
-# Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
- set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-set :sidekiq_config, "#{current_path}/config/sidekiq.yml"
-set :sidekiq_processes, 2
-set :sidekiq_log, "#{current_path}/log/sidekiq.log"
-set :sidekiq_role, :sidekiq
-
-set :puma_preload_app, true
-set :puma_init_active_record, true
-set :puma_bind,       "unix:///var/www/pushkin-contest-bot/shared/tmp/sockets/puma.sock"
-set :puma_state,      "var/www/pushkin-contest-bot/shared/tmp/pids/puma.state"
-set :puma_pid,        "var/www/pushkin-contest-bot/shared/tmp/pids/puma.pid"
-
-
-
+# Don't change these unless you know what you're doing
+set :pty,             true
+set :use_sudo,        false
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :ssh_options,     { forward_agent: true }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using 
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
+## Defaults:
+# set :scm,           :git
+# set :branch,        :master
+# set :format,        :pretty
+# set :log_level,     :debug
+# set :keep_releases, 5
+
+## Linked Files & Directories (Default None):
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
-desc 'Create Directories for Puma Pids and Socket'
-task :make_dirs do
-on roles(:app) do
-execute "mkdir #{shared_path}/tmp/sockets -p"
-execute "mkdir #{shared_path}/tmp/pids -p"
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
 end
-end
-before :start, :make_dirs
-end
+
 namespace :deploy do
-desc "Make sure local git is in sync with remote."
-task :check_revision do
-on roles(:app) do
-unless `git rev-parse HEAD` == `git rev-parse origin/master`
-puts "WARNING: HEAD is not the same as origin/master"
-puts "Run `git push` to sync changes."
-exit
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      Rake::Task["puma:start"].reenable
+      Rake::Task["puma:restart"].reenable
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
 end
-end
-end
-desc 'Initial Deploy'
-task :initial do
-on roles(:app) do
-before 'deploy:restart', 'puma:start'
-invoke 'deploy'
-end
-end
-desc 'Restart application'
-task :restart do
-on roles(:app), in: :sequence, wait: 5 do
-invoke 'puma:restart'
-end
-end
-before :starting,     :check_revision
-after  :finishing,    :compile_assets
-after  :finishing,    :cleanup
-after  :finishing,    :restart
-end
+
 # ps aux | grep puma    # Get puma pid
 # kill -s SIGUSR2 pid   # Restart puma
 # kill -s SIGTERM pid   # Stop puma
-
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
