598749386e41c9d046e59593c4042df805278479
ilya-github <kulikovitch52@mail.ru>
2017-12-24 05:27:30 +0300

    sd

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,10 @@
+
+set :branch, 'master'
+set :rails_env, 'production'
+set :unicorn, 'production'
+
+set :puma_threads, [4,12]
+set :puma_workers, 2
 server '92.53.91.93', user: 'deployer', roles: %w{app web db sidekiq}
 # server-based syntax
 # ======================
