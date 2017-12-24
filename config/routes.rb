212b2e4f67f3a3fe52b7d5b58aaa99cba189bc94
ilya-github <kulikovitch52@mail.ru>
2017-12-24 19:56:23 +0300

    sdsd

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     get 'verse/index'
     get '/quiz', to: 'verse#index'
+    post '/quiz', to: 'verse#index'
   root 'verse#created_verse'
 end
