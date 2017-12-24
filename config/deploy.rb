249d7e0beaf2c0b0349bf1c355463d84e03281c3
ilya-github <kulikovitch52@mail.ru>
2017-12-24 04:10:08 +0300

    dssd

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,6 +41,56 @@ set :puma_state,      "var/www/pushkin-contest-bot/shared/tmp/pids/puma.state"
 set :puma_pid,        "var/www/pushkin-contest-bot/shared/tmp/pids/puma.pid"
 
 
+
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using 
+
+
+namespace :puma do
+desc 'Create Directories for Puma Pids and Socket'
+task :make_dirs do
+on roles(:app) do
+execute "mkdir #{shared_path}/tmp/sockets -p"
+execute "mkdir #{shared_path}/tmp/pids -p"
+end
+end
+before :start, :make_dirs
+end
+namespace :deploy do
+desc "Make sure local git is in sync with remote."
+task :check_revision do
+on roles(:app) do
+unless `git rev-parse HEAD` == `git rev-parse origin/master`
+puts "WARNING: HEAD is not the same as origin/master"
+puts "Run `git push` to sync changes."
+exit
+end
+end
+end
+desc 'Initial Deploy'
+task :initial do
+on roles(:app) do
+before 'deploy:restart', 'puma:start'
+invoke 'deploy'
+end
+end
+desc 'Restart application'
+task :restart do
+on roles(:app), in: :sequence, wait: 5 do
+invoke 'puma:restart'
+end
+end
+before :starting,     :check_revision
+after  :finishing,    :compile_assets
+after  :finishing,    :cleanup
+after  :finishing,    :restart
+end
+# ps aux | grep puma    # Get puma pid
+# kill -s SIGUSR2 pid   # Restart puma
+# kill -s SIGTERM pid   # Stop puma
+
+
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
 
